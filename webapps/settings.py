51a49bf34e658910bb59ee53c7e29945c4dc085e
Ubuntu <ubuntu@ip-172-31-28-233.us-east-2.compute.internal>
2018-04-15 23:48:50 +0000

    configured sql database

--- webapps/settings.py
+++ webapps/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'webapps.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	 'ENGINE': 'django.db.backends.mysql',
+	 'NAME': 'django',
+	 'USER': 'root',
+	 'PASSWORD': 'couchpotato%735',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
